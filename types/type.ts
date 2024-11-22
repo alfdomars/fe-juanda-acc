341a885397739c9078d5538d05731a45ce4c4d07
Alfdonyus Marschaal <alfdonyusmarschaal@gmail.com>
2024-11-22 17:13:06 +0700

    new

--- types/type.ts
+++
@@ -1,8 +0,0 @@
-// types.ts (di dalam folder types jika Anda ingin menggunakan tipe untuk data)
-export type Title = {
-  id: string;
-  name: string;
-  description: string;
-  createdAt: string;
-  updatedAt: string;
-};
